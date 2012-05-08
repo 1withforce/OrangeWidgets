a09a5cd2b10afac3e5d789dedf21fbe339687a4f
KingLando <benjamin.ringel@ncf.edu>
2012-05-08 16:56:27 -0300

    Update DataWidget.py

--- DataWidget.py
+++ DataWidget.py
@@ -2,7 +2,7 @@
 <name>Fasta Converter</name>
 <description>Converts Fasta files with biological sequences into tab files</description>
 <icon>icons/fasta.gif</icon>
-<priority>1000</priority>
+<priority>1</priority>
 """
 
 from OWWidget import *
@@ -10,11 +10,9 @@ import OWGUI
 import re
 
 class OWFastaConverter(OWWidget):
-    
     def __init__(self, parent=None, signalManager=None):
-		OWWidget.__init__(self, parent, signalManager, 'new_tab_file')
-        
-       	self.inputs = [("convertFastaTab", ExampleTable, self.convertFastaTab)]
+        OWWidget.__init__(self, parent, signalManager, 'FastaConverter')
+        self.inputs = [("convertFastaTab", ExampleTable, self.convertFastaTab)]
         self.outputs = [("Tab File", ExampleTable)]
 
         # GUI
@@ -22,41 +20,38 @@ class OWFastaConverter(OWWidget):
         self.infoa = OWGUI.widgetLabel(box, 'No fasta file yet, waiting to get something.')
         self.infob = OWGUI.widgetLabel(box, '')
         self.resize(100,50)
-		
-        tab_file = regex_function.ExapmleTable('')
-	def convertFastaTab(self, dataset):
+    def convertFastaTab(self, dataset):
         if dataset:        
-        	def cleanup(b_array):##eliminates empty array that results from re.split
-                	return b_array[1:]
+            def cleanup(b_array):##eliminates empty array that results from re.split
+                return b_array[1:]
         
-        	input_file=open("{}".format(dataset), 'r')
-        	primary_input=input_file.read()##primary_input is now a string containing the original fasta data
-        	headpattern= re.compile(">(.+)$", re.M)#pattern for isolating the header
-        	filepat=re.compile(r"(.+)\..+")##pattern for finding the file path no matter the extension-may cause a crash if directories have periods in their names
-        	input_filename=filepat.findall(dataset)[0]
-        	headers = headpattern.findall(primary_input)
-        	bodies = re.split(">.*\\n", primary_input)##pattern for isolating bodies (sequences)
-        	bodies = cleanup(bodies)
-        
-        	clean_bodies1=[]
-       		clean_bodies2=[]
+            input_file=open("{}".format(dataset), 'r')
+            primary_input=input_file.read()##primary_input is now a string containing the original fasta data
+            headpattern= re.compile(">(.+)$", re.M)#pattern for isolating the header
+            filepat=re.compile(r"(.+)\..+")##pattern for finding the file path no matter the extension-may cause a crash if directories have periods in their names
+            input_filename=filepat.findall(dataset)[0]
+            headers = headpattern.findall(primary_input)
+            bodies = re.split(">.*\\n", primary_input)##pattern for isolating bodies (sequences)
+            bodies = cleanup(bodies)
 
-        	##below eliminate all whitespaces that may have found their way into the sequences
-        	for i in bodies:
-                	clean_bodies1.append(re.sub("\\n", "", i))##may be redundant
-        	for i in clean_bodies1:
-                	clean_bodies2.append(i.strip(" \t\n\r"))
-                
-        	newstring="Description\tSequence Data\n\n"
+            self.clean_bodies1=[]
+            self.clean_bodies2=[]
+
+            for i in bodies:
+                self.clean_bodies1.append(re.sub("\\n", "", i))##may be redundant
+
+            for i in clean_bodies1:
+            	self.clean_bodies2.append(i.strip(" \t\n\r"))
+
+            self.newstring="Description\tSequence Data\n\n"
+            for i in range(len(clean_bodies2)):
+                self.newstring=newstring+headers[i]+"\t"+self.clean_bodies2[i]+"\n"##Format: head1<tab>body1<newline>...
+            #print newstring
         
-        	for i in range(len(clean_bodies2)):
-                	newstring=newstring+headers[i]+"\t"+clean_bodies2[i]+"\n"##Format: head1<tab>body1<newline>...
-        #print newstring
-        
-        	output=open("{}.tab".format(input_filename), 'w')
-        	final = output.write(newstring)
+        	self.output=open("{}.tab".format(input_filename), 'w')
+        	self.final = output.write(newstring)
         	self.infob.setText("File converted, saved as {}.tab".format(input_filename))
-			self.send('Tab File', final)
+        	self.send('Tab File', self.final)
         else:
             self.infoa.setText('No data on input yet, waiting to get something.')
             self.infob.setText('')
